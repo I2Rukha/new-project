38284ae62fcf6a1d99ea2a6197a9e6ba664c48c3
Ivan Rukha <rukha02@mail.ru>
2024-07-24 19:16:58 +0300

    second

--- git/src/Main.java
+++ git/src/Main.java
@@ -1,11 +1,8 @@
 public class Main {
 
-    static String s;
     public static void main(String[] args) {
 
-        s = "Hello World";
-
-        System.out.println(s);
+        System.out.println("Hello World!");
 
     }
 }
